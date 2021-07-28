676052b110540b18649412d2da84c75452593706
NIHARIKA983 <niharikagowda1996@gmail.com>
2021-07-28 18:27:24 +0530

    Added:User need to enter a valid Last Name

--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -10,5 +10,27 @@ public class UserRegistrationTest {
         boolean result = validator.validateFirstName("Niharika");
         Assert.assertTrue(result);
     }
+
+    @Test
+    public void givenFirstName_whenInValid_thenReturnFalse() {
+        UserRegistration validator = new UserRegistration();
+        boolean result = validator.validateFirstName("niha");
+        Assert.assertFalse(result);
+
+    }
+
+    @Test
+    public void givenLastName_whenValid_thenReturnTrue() {
+        UserRegistration validator = new UserRegistration();
+        boolean result = validator.validateFirstName("Gowda");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenLastName_whenInValid_thenReturnFalse() {
+        UserRegistration validator = new UserRegistration();
+        boolean result = validator.validateFirstName("kv");
+        Assert.assertFalse(result);
+    }
 }
 
